fd9299b5c517e306edfb70cf7d98c313aead03f7
TX1150 <acglogin@qq.com>
2019-01-06 17:01:38 +0800

    修改测试
    main.c添加一行注释

--- main.c
+++ main.c
@@ -1,32 +1,32 @@
 #include<stdio.h>
 #include<aes.h>
 #pragma comment(lib,"mbedTLS.lib")
-
-int main(void)
+//ä¿®æ”¹æ³¨é‡Š
+int   mainï¼ˆvoidï¼‰
 {
 	mbedtls_aes_context aes_ctx;
-	//ÃÜÔ¿ÊıÖµ  
+	//å¯†é’¥æ•°å€¼  
 	unsigned char key[16] = { 0x01,0x02,0x03,0x04,0x06,0x06,0x07,0x08,0x09,0x0A,0x0B,0x0C,0x0D,0x0E,0x0F,0x00 };
-	//Ã÷ÎÄ¿Õ¼ä  
+	//æ˜æ–‡ç©ºé—´  
 	unsigned char plain[16] = "aaaaaaaaaa";
-	//½âÃÜºóÃ÷ÎÄµÄ¿Õ¼ä  
+	//è§£å¯†åæ˜æ–‡çš„ç©ºé—´  
 	unsigned char dec_plain[16] = { 0 };
-	//ÃÜÎÄ¿Õ¼ä  
+	//å¯†æ–‡ç©ºé—´  
 	unsigned char cipher[16] = { 0 };
 	mbedtls_aes_init(&aes_ctx);
-	//ÉèÖÃ¼ÓÃÜÃÜÔ¿  
+	//è®¾ç½®åŠ å¯†å¯†é’¥  
 	mbedtls_aes_setkey_enc(&aes_ctx, key, 256);
-	printf("\n*********** ¼ÓÃÜÇ°£º%s\n", plain);
+	printf("\n*********** åŠ å¯†å‰ï¼š%s\n", plain);
 	mbedtls_aes_crypt_ecb(&aes_ctx, MBEDTLS_AES_ENCRYPT, plain, cipher);
-	//printf("\n*********** ¼ÓÃÜºó£º%s\n", cipher);
+	//printf("\n*********** åŠ å¯†åï¼š%s\n", cipher);
 	printf("\n");
 	for (size_t i = 0; i < 10; i++)
 		printf("%x\t", cipher[i]);
 	printf("\n");
-	//ÉèÖÃ½âÃÜÃÜÔ¿  
+	//è®¾ç½®è§£å¯†å¯†é’¥  
 	mbedtls_aes_setkey_dec(&aes_ctx, key, 256);
 	mbedtls_aes_crypt_ecb(&aes_ctx, MBEDTLS_AES_DECRYPT, cipher, dec_plain);
-	printf("\n*********** ½âÃÜºó£º%s\n", dec_plain);
+	printf("\n*********** è§£å¯†åï¼š%s\n", dec_plain);
 	mbedtls_aes_free(&aes_ctx);
 	system("pause");
 	return 0;
